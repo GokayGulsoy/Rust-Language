444e1eeb49e44518e34926eac6b6da8e5a18b9bb
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2023-11-29 00:26:44 +0300

    Delete writing_automated_tests directory to update it with correct version

--- writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.d
+++
@@ -1,5 +0,0 @@
-/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.rmeta: src/main.rs
-
-/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.d: src/main.rs
-
-src/main.rs:
