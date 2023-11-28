8eb37ef438891b18ea0b99252081105b12d6c91b
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2023-11-29 00:25:39 +0300

    Writing automated tests uploaded

---
+++ writing_automated_tests/target/debug/deps/writing_automated_tests-34d970bc5cded19d.d
@@ -0,0 +1,5 @@
+/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-34d970bc5cded19d.rmeta: src/main.rs
+
+/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-34d970bc5cded19d.d: src/main.rs
+
+src/main.rs:
---
+++ writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.d
@@ -0,0 +1,5 @@
+/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.rmeta: src/main.rs
+
+/home/gokay/Desktop/Rust Language/writing_automated_tests/target/debug/deps/writing_automated_tests-45e5e6105290a8b0.d: src/main.rs
+
+src/main.rs:
